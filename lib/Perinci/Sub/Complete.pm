8cd4bb7b5f016436d63020899fd28a33b26779e8
perlancar (on pc-office) <perlancar@gmail.com>
2019-02-14 01:46:40 +0700

    [logging] Log gen_completion() arguments

--- lib/Perinci/Sub/Complete.pm
+++ lib/Perinci/Sub/Complete.pm
@@ -144,7 +144,7 @@ sub complete_from_schema {
                     my $mod_pm = $mod; $mod_pm =~ s!::!/!g; $mod_pm .= ".pm";
                     require $mod_pm;
                     my $fref = \&{"$mod\::gen_completion"};
-                    log_trace("[comp][periscomp] invoking gen_completion() from %s ...", $mod);
+                    log_trace("[comp][periscomp] invoking %s's gen_completion(%s) ...", $mod, $xcargs);
                     $comp = $fref->(%$xcargs);
                 } else {
                     log_trace("[comp][periscomp] module %s is not installed, skipped", $mod);
